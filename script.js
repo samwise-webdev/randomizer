9cbbafa5b17694b94deac72fb6b4ee6d6ac0f933
Sam Burgett <samwise.burgett@outlook.com>
2023-12-01 17:22:47 -0500

    added ids and started randomizer code

---
+++ script.js
@@ -0,0 +1,19 @@
+const bgColorBtn = document.querySelector('#bgBtn')
+const bgStyleBtn = documet.querySelector('#styleBtn')
+
+bgColorBtn.addEventListener('click', function() {
+    const newColor = randomColor();
+    document.body.style.backgroundColor = newColor;
+})
+
+
+function btnStyle() {
+
+}
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
